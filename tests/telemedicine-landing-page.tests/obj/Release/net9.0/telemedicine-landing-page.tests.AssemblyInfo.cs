6640788abcf46d47c619820ddfbcd168fd9ad6d2
Khang Truong <khangtruongminh200804@gmail.com>
2026-05-19 00:59:15 +0700

    feat(ui): thêm loading spinner cho Login/Register, toast khi gán vai trò thành công

--- tests/telemedicine-landing-page.tests/obj/Release/net9.0/telemedicine-landing-page.tests.AssemblyInfo.cs
+++ tests/telemedicine-landing-page.tests/obj/Release/net9.0/telemedicine-landing-page.tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("telemedicine-landing-page.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd7c8346add2f4c2cc9b128e1318d0cc0a6fa2c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50bc76470e4366e270f54b546bcc25e2dd266363")]
 [assembly: System.Reflection.AssemblyProductAttribute("telemedicine-landing-page.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("telemedicine-landing-page.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
