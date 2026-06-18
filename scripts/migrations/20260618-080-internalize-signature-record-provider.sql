be6b50eb7eb8753bf4e01cfa298cd2f8a57a7b26
Khang Truong <khangtruongminh200804@gmail.com>
2026-06-18 14:46:57 +0700

    fix(signatures): preserve immutable signature audit records

--- scripts/migrations/20260618-080-internalize-signature-record-provider.sql
+++ scripts/migrations/20260618-080-internalize-signature-record-provider.sql
@@ -8,9 +8,5 @@ BEGIN
 
     ALTER TABLE med.signature_records
         ADD CONSTRAINT DF_signature_records_provider DEFAULT N'internal' FOR provider_code;
-
-    UPDATE med.signature_records
-    SET provider_code = N'internal'
-    WHERE provider_code = N'demo';
 END
 GO
